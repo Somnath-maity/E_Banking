26e2f6e4e766f3b9b5db2ba013eb4f40e638bf05
Somnath <Somnathmaityik@gmail.com>
2022-03-20 18:34:10 +0530

    This Android Application allows users to transfer money to different users.

--- app/src/main/java/com/example/basicbankingapp/DB/UserHelper.java
+++ app/src/main/java/com/example/basicbankingapp/DB/UserHelper.java
@@ -14,11 +14,8 @@ public class UserHelper extends SQLiteOpenHelper {
 
     String TABLE_NAME = UserEntry.TABLE_NAME;
 
-    /** Name of the database file */
     private static final String DATABASE_NAME = "User.db";
 
-    /**
-     * Database version. If you change the database schema, you must increment the database version.*/
     private static final int DATABASE_VERSION = 1;
 
     public UserHelper(Context context) {
@@ -27,7 +24,6 @@ public class UserHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        // Create a String that contains the SQL statement to create the pets table
         String SQL_CREATE_USER_TABLE =  "CREATE TABLE " + UserEntry.TABLE_NAME + " ("
                 + UserEntry.COLUMN_USER_ACCOUNT_NUMBER + " INTEGER, "
                 + UserEntry.COLUMN_USER_NAME + " VARCHAR, "
@@ -36,25 +32,18 @@ public class UserHelper extends SQLiteOpenHelper {
                 + UserEntry.COLUMN_USER_PHONE_NO + " VARCHAR, "
                 + UserEntry.COLUMN_USER_ACCOUNT_BALANCE + " INTEGER NOT NULL);";
 
-        // Execute the SQL statement
         db.execSQL(SQL_CREATE_USER_TABLE);
 
-        // Insert Into Table
-        db.execSQL("insert into " + TABLE_NAME + " values(7860,'Tanishq Saini', 'tanishq@gmail.com','7584','7895641238', 15000)");
-        db.execSQL("insert into " + TABLE_NAME + " values(5862,'Gagan Tripathi', 'gagan@gmail.com','1258','8995641238', 5000)");
-        db.execSQL("insert into " + TABLE_NAME + " values(7895,'Surya Pratap', 'surya@gmail.com','8896','7595645896', 1000)");
-        db.execSQL("insert into " + TABLE_NAME + " values(1258,'Vikram Garasiya', 'vikram@gmail.com','7752','9995640038', 8000)");
-        db.execSQL("insert into " + TABLE_NAME + " values(7410,'Shivani Kumari', 'shivani@gmail.com','3669','9095648962', 7500)");
-        db.execSQL("insert into " + TABLE_NAME + " values(8529,'Piyush Joshi', 'piyush@gmail.com','9985','8855640238', 6500)");
-        db.execSQL("insert into " + TABLE_NAME + " values(3698,'Yash Pratap', 'yash@gmail.com','1207','8895640215', 4500)");
-        db.execSQL("insert into " + TABLE_NAME + " values(7853,'Khushi Jain', 'khushi@gmail.com','4522','9985021539', 2500)");
-        db.execSQL("insert into " + TABLE_NAME + " values(4562,'Ritik Sharma', 'ritik@gmail.com','6582','9309565238', 10500)");
-        db.execSQL("insert into " + TABLE_NAME + " values(2365,'Rohit Patidar', 'rohit@gmail.com','5450','8292591201', 9900)");
-        db.execSQL("insert into " + TABLE_NAME + " values(7854,'Anurag Sharma', 'anurag@gmail.com','2656','9015641200', 9800)");
-        db.execSQL("insert into " + TABLE_NAME + " values(3621,'Hitish Kumar', 'hitish@gmail.com','1203','9995641999', 11000)");
-        db.execSQL("insert into " + TABLE_NAME + " values(1122,'Naveen Chaturvedi', 'naveen@gmail.com','5566','9119541001', 5800)");
-        db.execSQL("insert into " + TABLE_NAME + " values(9512,'Gauri Parashar', 'gauri@gmail.com','2236','6254642205', 3500)");
-        db.execSQL("insert into " + TABLE_NAME + " values(7530,'Farhan Khan', 'farhan@gmail.com','6692','6893641266', 1010)");
+        db.execSQL("insert into " + TABLE_NAME + " values(5660,'Sahil Bohra', 'sahilbohra@ebanking.com','7954','7875621238', 600)");
+        db.execSQL("insert into " + TABLE_NAME + " values(5856,'Dipankar Ghosh', 'dghosh@ebanking.com','1958','89956787562', 200)");
+        db.execSQL("insert into " + TABLE_NAME + " values(9595,'Aryan Kumar', 'aryankumar@ebanking.com','9596','6778756896', 544)");
+        db.execSQL("insert into " + TABLE_NAME + " values(1958,'Rashi Panchal', 'rashipanchal@ebanking.com','6552','6978756238', 600)");
+        db.execSQL("insert into " + TABLE_NAME + " values(9510,'Ved Rathore', 'vedrathore@ebanking.com','2369','8875628962', 800)");
+        db.execSQL("insert into " + TABLE_NAME + " values(8595,'Dipali Chauhan', 'Dipalichauhan@ebanking.com','95785','8787562238', 800)");
+        db.execSQL("insert into " + TABLE_NAME + " values(9598,'Pravin Agarwal', 'pravinagrawal@ebanking.com','9507','5878756215', 900)");
+        db.execSQL("insert into " + TABLE_NAME + " values(7895,'Riya Jain', 'riyajainv','4522','6775621539', 780)");
+        db.execSQL("insert into " + TABLE_NAME + " values(9562,'Riyanchal Sharma', 'rsharma@ebanking.com','6952','6975625238', 1500)");
+
     }
 
     @Override
--- app/src/main/java/com/example/basicbankingapp/UI/HomeScreen.java
+++ app/src/main/java/com/example/basicbankingapp/UI/HomeScreen.java
@@ -6,7 +6,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -25,24 +24,18 @@ public class HomeScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
 
-        anim = AnimationUtils.loadAnimation(this, R.anim.animation);
         bankLogo = findViewById(R.id.bank_logo);
-        btnAllUsers = findViewById(R.id.all_users);
-        btnAllTransactions = findViewById(R.id.all_transactions);
-        tvBankTitle = findViewById(R.id.bank_title);
+        btnAllUsers = findViewById(R.id.users);
+        btnAllTransactions = findViewById(R.id.transactions);
 
-        bankLogo.setAnimation(anim);
-        tvBankTitle.setAnimation(anim);
-        btnAllUsers.setAnimation(anim);
-        btnAllTransactions.setAnimation(anim);
     }
 
-   public void showAllUsers(View view) {
+   public void Users(View view) {
         Intent intent = new Intent(this, UsersList.class);
         startActivity(intent);
     }
 
-    public void showAllTransactions(View view) {
+    public void Transactions(View view) {
         Intent intent = new Intent(this, TransactionHistory.class);
         startActivity(intent);
     }
